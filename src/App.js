489f84862f92083b641d07b93b5a8fa2b0765e74
MaanilVerma <MaanilVerma@users.noreply.github.com>
2021-05-28 21:06:06 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React, { useRef, useState } from 'react';
 import './App.css';
 
@@ -10,12 +11,13 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-    apiKey: "AIzaSyB8YiPJrZB_glv828TCVrQRhv8072hZ3DI",
-    authDomain: "chatapp-c8883.firebaseapp.com",
-    projectId: "chatapp-c8883",
-    storageBucket: "chatapp-c8883.appspot.com",
-    messagingSenderId: "896627883586",
-    appId: "1:896627883586:web:52c1656a4aee16e28177db"
+  apiKey: "AIzaSyB-o9DAjNYOUj9zJO6T5jKLxnw9BlDW4mc",
+  authDomain: "superchat-313e4.firebaseapp.com",
+  projectId: "superchat-313e4",
+  storageBucket: "superchat-313e4.appspot.com",
+  messagingSenderId: "144627506932",
+  appId: "1:144627506932:web:489649467878e2a2bd48a1",
+  measurementId: "G-LVKLLPE83T"
 })
 
 const auth = firebase.auth();
